04a21e19df9795bcf3c20b02cb762a959be3e603
Leo Kenzley <leokenzley@gmail.com>
2025-09-25 19:06:53 -0300

    feat: ajustando o logout

--- src/main/java/com/leokenzley/bank_transactions/controller/AuthenticationController.java
+++ src/main/java/com/leokenzley/bank_transactions/controller/AuthenticationController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -28,7 +29,7 @@ public class AuthenticationController {
     return "/init";
   }
 
-  @GetMapping("/logout")
+  @PostMapping("/logout")
   public String logout() {
     return "/authentication/login";
   }
